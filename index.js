36c48f24fab72dfe24ccb8e3489725906e7903ad
lee mwangi <lee.mwangi1@student.moringaschool.com>
2024-06-28 11:47:31 +0300

    commiting index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+// Initial employee object
+let employee = { name: 'Sam' };
+
+// Function to update an employee with a key and value non-destructively
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return { ...employee, [key]: value };
+}
+
+// Function to update an employee with a key and value destructively
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+// Function to delete a key from an employee non-destructively
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+// Function to delete a key from an employee destructively
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
